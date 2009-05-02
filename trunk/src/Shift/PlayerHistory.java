0fc36d8d227dd4269bab50f2f6fc3c2eb587d90c
mattcrain <mattcrain@54a21e48-2926-11de-8010-b76041e17fd7>
2009-05-02 23:12:41 +0000

    Fixed an error in Ucigame collided() for action CHECK. Started creating a custom simple collision direction detection. Still having issues, but rewinding is still awesome.

--- trunk/src/Shift/PlayerHistory.java
+++ trunk/src/Shift/PlayerHistory.java
@@ -2,15 +2,18 @@ package Shift;
 
 public class PlayerHistory
 {
-	double xLoc, yLoc;
+	double xLoc, yLoc, xSpeed, ySpeed;
 	int frame;
 	boolean flipHoriz, flipVert;
 	Actions action;
 	
-	public PlayerHistory(double xLoc, double yLoc, int frame, boolean flipHoriz, boolean flipVert, Actions action)
+	public PlayerHistory(double xLoc, double yLoc, double xSpeed, double ySpeed,
+			int frame, boolean flipHoriz, boolean flipVert, Actions action)
 	{
 		this.xLoc = xLoc;
 		this.yLoc = yLoc;
+		this.xSpeed = xSpeed;
+		this.ySpeed = ySpeed;
 		this.frame = frame;
 		this.flipHoriz = flipHoriz;
 		this.flipVert = flipVert;
