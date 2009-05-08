c1ad33fac4ca85be3403644248311868a8720b95
mattcrain <mattcrain@54a21e48-2926-11de-8010-b76041e17fd7>
2009-05-08 18:28:10 +0000

    Started work on Inventory and Inventory display.  Isn't fully functioning yet, but it doesn't break anything else.

---
+++ src/Shift/Inventory.java
@@ -0,0 +1,27 @@
+package Shift;
+
+public class Inventory 
+{
+	InventoryItem[] items;
+	private static final int IMG_X_OFFSET = 10 + InventoryItem.WIDTH;
+	
+	public Inventory(Shift parent)
+	{
+		items = new InventoryItem[3];
+		int yLoc = 10;
+		int xLoc = Shift.WIDTH - IMG_X_OFFSET;
+		items[0] = new InventoryItem(parent, "items/keyInventory.png", xLoc, yLoc);
+		xLoc -= IMG_X_OFFSET;
+		items[1] = new InventoryItem(parent, "items/keyInventory.png", xLoc, yLoc);
+		xLoc -= IMG_X_OFFSET;
+		items[2] = new InventoryItem(parent, "items/keyInventory.png", xLoc, yLoc);
+	}
+	
+	public void draw()
+	{
+		items[0].draw();
+		items[1].draw();
+		items[2].draw();
+	}
+
+}
