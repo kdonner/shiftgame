64f38b5d35292fb3f18f219c0bee0789f2fcd1a7
mattcrain <mattcrain@54a21e48-2926-11de-8010-b76041e17fd7>
2009-05-24 00:23:42 +0000

    Added falling damage

--- trunk/src/Shift/Dimensions.java
+++ trunk/src/Shift/Dimensions.java
@@ -7,14 +7,14 @@ public enum Dimensions {
 	DIM1 ((Constants.GRAVITY / Shift.FRAME_RATE), true, 20, 1, new float[]{0.9f, 0.9f, 0.9f, 1f}),
 	DIM2 ((Constants.GRAVITY / Shift.FRAME_RATE) / 4, true, 20, 2, new float[]{0.5f, 0.5f, 1f, 1f}),
 	DIM3 ((Constants.GRAVITY / Shift.FRAME_RATE) * 4, true, 20, 3, new float[]{1f, 0.5f, 0.5f, 1f}),
-	DIM4 ((Constants.GRAVITY / Shift.FRAME_RATE), false, 20, 4, new float[]{0.0f, 0.9f, 0.7f, 1f}),
+	DIM4 ((Constants.GRAVITY / Shift.FRAME_RATE), false, -20, 4, new float[]{0.0f, 0.9f, 0.7f, 1f}),
 	DIM5 ((Constants.GRAVITY / Shift.FRAME_RATE), true, 20, 5, new float[]{0f, 0f, 0f, 1f}),
 	DIM6 ((Constants.GRAVITY / Shift.FRAME_RATE) / 2, true, 20, 6, new float[]{0f, 0f, 0f, 1f}),
-	DIM7 ((Constants.GRAVITY / Shift.FRAME_RATE) / 4, false, 20, 7, new float[]{0f, 0f, 0f, 1f}),
-	DIM8 ((Constants.GRAVITY / Shift.FRAME_RATE) * 4, false, 20, 8, new float[]{0f, 0f, 0f, 1f});
+	DIM7 ((Constants.GRAVITY / Shift.FRAME_RATE) / 4, false, -20, 7, new float[]{0f, 0f, 0f, 1f}),
+	DIM8 ((Constants.GRAVITY / Shift.FRAME_RATE) * 4, false, -20, 8, new float[]{0f, 0f, 0f, 1f});
 	
 	private final double gravity;
-	private final boolean gravIsDown;
+	final boolean gravIsDown;
 	int dimNum;
 	float[] filters;
 	final double terminalVelocity;
