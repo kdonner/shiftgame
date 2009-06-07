b9068e8396e1ae094c40a93135824d237db91d0c
mattcrain <mattcrain@54a21e48-2926-11de-8010-b76041e17fd7>
2009-06-07 16:34:59 +0000

    Started basic audio integration

--- trunk/src/Shift/Player.java
+++ trunk/src/Shift/Player.java
@@ -365,6 +365,7 @@ public class Player extends Sprite
 		checkSpriteForCollision(d.door, false);
 		if(inven.items[0].hasFound && collided())
 		{
+			parent.getSound(Constants.AUDIO_DIR + "doorOpen.mp3");
 			d.retract();
 			inven.items[0].use();
 		}
